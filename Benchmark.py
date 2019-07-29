1f2e05d99ffdeac8bffd00d2cf87db5005742116
pete meltzer <meltzerpete@gmail.com>
2019-07-29 16:11:18 +0100

    running benchmark

---
+++ Benchmark.py
@@ -0,0 +1,163 @@
+import json
+import os.path
+import csv
+import time
+import numpy as np
+import torch
+from sklearn import metrics
+from sklearn.metrics import accuracy_score
+from sklearn.model_selection import StratifiedKFold, StratifiedShuffleSplit
+from torch.nn import CrossEntropyLoss
+from torch_geometric.data import DataLoader
+from torch_geometric.datasets import TUDataset
+
+from PiNet import PiNet
+
+
+def train():
+    model.train()
+
+    loss_all = 0
+    for data in train_loader:
+        data = data.to(device)
+        optimizer.zero_grad()
+        output = model(data)
+        label = data.y.to(device)
+        loss = crit(output, label)
+        loss.backward()
+        loss_all += data.num_graphs * loss.item()
+        optimizer.step()
+    return loss_all / len(dataset)
+
+
+def evaluate(loader):
+    model.eval()
+
+    predictions = []
+    labels = []
+
+    with torch.no_grad():
+        for data in loader:
+            data = data.to(device)
+            pred = model(data).detach().cpu().numpy()
+
+            label = data.y.detach().cpu().numpy()
+            predictions.append(pred)
+            labels.append(label)
+
+    labels = np.concatenate(labels)
+    predictions = np.concatenate(predictions).argmax(axis=1)
+
+    return accuracy_score(labels, predictions), metrics.confusion_matrix(labels, predictions).ravel()
+
+
+def get_experiment_count(file):
+    if os.path.isfile(file):
+        count = json.load(open(file, 'r')) + 1
+    else:
+        count = 0
+    json.dump(count, open(file, 'w'))
+    return count
+
+
+def get_splits():
+    return StratifiedKFold(n_splits=10).split(np.zeros([len(dataset), 1]), dataset.data.y)
+
+
+if __name__ == '__main__':
+    experiment_count = get_experiment_count('benchmark.count')
+
+    log_file = open('benchmark.log', 'a')
+    writer = csv.writer(log_file)
+
+    datasets = ['MUTAG', 'PTC_MM', 'PTC_MR', 'PTC_FM', 'PTC_FR', 'NCI1', 'NCI109', 'PROTEINS']
+    models = [
+        {
+            'class': PiNet,
+            'params': {
+                'message_passing': 'GCN',
+                'GCN_improved': True
+            }
+        },
+        {
+            'class': PiNet,
+            'message_passing': 'GCN',
+            'params': {
+                'GCN_improved': False
+            },
+        },
+        {
+            'class': PiNet,
+            'message_passing': 'GAT',
+            'params': {
+                'GAT_heads': [2, 2]
+            },
+        },
+        {
+            'class': PiNet,
+            'message_passing': 'GAT',
+            'params': {
+                'GAT_heads': [3, 2]
+            },
+        },
+    ]
+
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    print(f'running on {device}')
+    writer.writerow([experiment_count,
+                     'dataset_name',
+                     'model',
+                     'model_params',
+                     'split',
+                     'epoch',
+                     'time_for_epoch(ms)',
+                     'train_loss',
+                     'train_acc', 'val_acc', 'test_acc',
+                     'train_conf', 'val_conf', 'test_conf'])
+    log_file.flush()
+
+    for dataset_name in datasets:
+        for model_dict in models:
+
+            dataset = TUDataset(root=f'data/{dataset_name}', name=dataset_name).shuffle()
+
+            model = model_dict['class'](dataset.num_features, 64, 64, dataset.num_classes, **model_dict['params']).to(
+                device)
+            optimizer = torch.optim.Adam(model.parameters(), lr=0.005, weight_decay=0.001)
+            crit = CrossEntropyLoss()
+
+            for split, (all_train_idx, test_idx) in enumerate(get_splits()):
+
+                # convert idx to torch tensors
+                all_train_idx = torch.tensor(all_train_idx, dtype=torch.long)
+                test_idx = torch.tensor(test_idx, dtype=torch.long)
+
+                train_idx, val_idx = next(StratifiedShuffleSplit(n_splits=1, test_size=0.1)
+                                          .split(np.zeros([len(all_train_idx), 1]), dataset.data.y[all_train_idx]))
+
+                train_idx = torch.tensor(train_idx, dtype=torch.long)
+                val_idx = torch.tensor(val_idx, dtype=torch.long)
+
+                train_loader = DataLoader(dataset[train_idx], batch_size=len(train_idx))
+                val_loader = DataLoader(dataset[val_idx], batch_size=len(val_idx))
+                test_loader = DataLoader(dataset[test_idx], batch_size=len(test_idx))
+
+                for epoch in range(300):
+                    start = time.time()
+                    train_loss = train()
+                    time_for_epoch = (time.time() - start) * 10e3
+                    train_acc, train_conf = evaluate(train_loader)
+                    val_acc, val_conf = evaluate(val_loader)
+                    test_acc, test_conf = evaluate(test_loader)
+
+                    writer.writerow([experiment_count,
+                                     dataset_name,
+                                     model_dict["class"].__name__,
+                                     model_dict['params'],
+                                     split,
+                                     epoch,
+                                     time_for_epoch,
+                                     train_loss,
+                                     train_acc, val_acc, test_acc,
+                                     train_conf, val_conf, test_conf])
+                    log_file.flush()
--- PiNet.py
+++ PiNet.py
@@ -1,33 +1,38 @@
-import numpy as np
 import torch
 import torch.nn.functional as F
-from torch.utils.data import random_split
-from sklearn import metrics
-from sklearn.metrics import accuracy_score
 from torch.nn import Module
-from torch_geometric.data import DataLoader
-from torch_geometric.datasets import TUDataset
-from torch_geometric.nn import GCNConv
+from torch_geometric.nn import GATConv
 from torch_geometric.utils import softmax, to_dense_batch
 
 
 class PiNet(Module):
 
-    def __init__(self, *dims):
+    def __init__(self, *dims, message_passing='GCN', GCN_improved=False, GAT_heads=None):
         super(PiNet, self).__init__()
-        self.gcn_a1 = GCNConv(dims[0], dims[1], improved=True)
-        self.gcn_a2 = GCNConv(dims[1], dims[2], improved=True)
-        self.gcn_x1 = GCNConv(dims[0], dims[1], improved=True)
-        self.gcn_x2 = GCNConv(dims[1], dims[2], improved=True)
-        self.linear2 = torch.nn.Linear(dims[2] ** 2, dims[-1])
+        if message_passing == 'GCN':
+            from torch_geometric.nn import GCNConv
+            self.mp_a1 = GCNConv(dims[0], dims[1], improved=GCN_improved)
+            self.mp_a2 = GCNConv(dims[1], dims[2], improved=GCN_improved)
+            self.mp_x1 = GCNConv(dims[0], dims[1], improved=GCN_improved)
+            self.mp_x2 = GCNConv(dims[1], dims[2], improved=GCN_improved)
+            self.linear2 = torch.nn.Linear(dims[2] ** 2, dims[-1])
+
+        elif message_passing == 'GAT':
+            if (GAT_heads == None):
+                GAT_heads = [5, 2]
+            self.mp_a1 = GATConv(dims[0], dims[1], heads=GAT_heads[0])
+            self.mp_a2 = GATConv(dims[1] * GAT_heads[0], dims[2], heads=GAT_heads[1])
+            self.mp_x1 = GATConv(dims[0], dims[1], heads=GAT_heads[0])
+            self.mp_x2 = GATConv(dims[1] * GAT_heads[0], dims[2], heads=GAT_heads[1])
+            self.linear2 = torch.nn.Linear((dims[2] * GAT_heads[1]) ** 2, dims[-1])
 
     def forward(self, data):
         x, edge_index, batch, num_graphs = data.x, data.edge_index, data.batch, data.num_graphs
 
-        a_1 = F.relu(self.gcn_a1(x, edge_index))
-        a_2 = softmax(self.gcn_a2(a_1, edge_index), batch)
-        x_1 = F.relu(self.gcn_x1(x, edge_index))
-        x_2 = F.relu(self.gcn_x2(x_1, edge_index))
+        a_1 = F.relu(self.mp_a1(x, edge_index))
+        a_2 = softmax(self.mp_a2(a_1, edge_index), batch)
+        x_1 = F.relu(self.mp_x1(x, edge_index))
+        x_2 = F.relu(self.mp_x2(x_1, edge_index))
 
         a_batch, _ = to_dense_batch(a_2, batch)
         a_t = a_batch.transpose(2, 1)
@@ -39,68 +44,3 @@ class PiNet(Module):
         final = F.softmax(batch_out, dim=-1)
 
         return final
-
-
-def train():
-    model.train()
-
-    loss_all = 0
-    for data in train_loader:
-        data = data.to(device)
-        optimizer.zero_grad()
-        output = model(data)
-        label = data.y.to(device)
-        loss = crit(output, label)
-        loss.backward()
-        loss_all += data.num_graphs * loss.item()
-        optimizer.step()
-    return loss_all / len(dataset)
-
-
-def evaluate(loader):
-    model.eval()
-
-    predictions = []
-    labels = []
-
-    with torch.no_grad():
-        for data in loader:
-            data = data.to(device)
-            pred = model(data).detach().cpu().numpy()
-
-            label = data.y.detach().cpu().numpy()
-            predictions.append(pred)
-            labels.append(label)
-
-    labels = np.concatenate(labels)
-    predictions = np.concatenate(predictions).argmax(axis=1)
-
-    return accuracy_score(labels, predictions), metrics.confusion_matrix(labels, predictions)
-
-
-dataset = TUDataset(root='data/proteins', name='PROTEINS').shuffle()
-device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-print(f'running on {device}')
-model = PiNet(dataset.num_features, 100, 64, dataset.num_classes).to(device)
-optimizer = torch.optim.Adam(model.parameters(), lr=0.005, weight_decay=0.001)
-crit = torch.nn.CrossEntropyLoss()
-
-tenth = int(len(dataset) / 10)
-
-data_train, val, test = random_split(dataset, [len(dataset) - tenth * 2, tenth, tenth])
-
-train_loader = DataLoader(data_train.dataset, batch_size=len(data_train.dataset))
-val_loader = DataLoader(val.dataset, batch_size=len(val.dataset))
-test_loader = DataLoader(test.dataset, batch_size=len(test.dataset))
-
-conf = None
-for epoch in range(200):
-    loss = train()
-    train_acc, train_conf = evaluate(train_loader)
-    val_acc, val_conf = evaluate(val_loader)
-    test_acc, test_conf = evaluate(test_loader)
-    print('Epoch: {:03d}, Loss: {:.5f}, Train Acc: {:.5f}, Val Acc: {:.5f}, Test Acc: {:.5f}'.
-          format(epoch, loss, train_acc, val_acc, test_acc))
-
-print('train\n', train_conf)
-print('test\n', test_conf)
