961ad134fbddc16b78370b48c7299ff9e187b053
pete meltzer <meltzerpete@gmail.com>
2019-07-30 07:05:20 +0100

    plot results - MISTAKE: ony first split from each trial is good - other splits use same weights from previous split :/

--- analysis/benchmark.py
+++ analysis/benchmark.py
@@ -1,3 +1,14 @@
 import pandas as pd
+import matplotlib.pyplot as plt
 
-df = pd.read_csv('../benchmark.log')
+df = pd.read_csv('benchmark.log')
+
+df = df.loc[df['split'] == 0]
+
+groups = df.groupby(['dataset_name', 'model_params'])
+for i, group_key in enumerate(groups.groups):
+    group = groups.get_group(group_key)
+    plt.plot(group['epoch'], group[['train_loss', 'train_acc', 'val_acc', 'test_acc']], '-')
+    plt.title(str(group_key))
+    plt.legend(['train_loss', 'train_acc', 'val_acc', 'test_acc'])
+    plt.show()
