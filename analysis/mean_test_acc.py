f74ce88384e039ea209ceb4a045aa4be9006dc10
pete meltzer <meltzerpete@gmail.com>
2019-08-07 14:50:44 +0100

    swap plots subplots

--- analysis/mean_test_acc.py
+++ analysis/mean_test_acc.py
@@ -13,15 +13,23 @@ test_accs = best.groupby(['dataset_name', 'model_params'])['test_acc']
 means = test_accs.mean().reset_index().groupby('dataset_name')
 stds = test_accs.std().reset_index().groupby('dataset_name')
 
-for group_key in means.groups:
-    fig = plt.figure()
-    plt.title(str(group_key))
+fig, nested_axes = plt.subplots(4, 2, sharex='all')
+axes = [ax for row in nested_axes for ax in row]
+
+fig.set_size_inches(8.27, 11.69)    # A4
+
+x_pos = np.arange(len(df['model_params'].unique()))
+xlabels = df['model_params'].apply(lambda line: line.replace('\',', '\'\n')).unique()
+plt.xticks(x_pos, xlabels, rotation=90)
+
+for ax, group_key in zip(axes, means.groups):
+    ax.set_title(str(group_key))
     _means = means.get_group(group_key)
     _stds = stds.get_group(group_key)
 
-    x_pos = np.arange(len(_means))
-    plt.bar(x_pos, _means['test_acc'], yerr=_stds['test_acc'])
+    ax.bar(np.arange(len(_means)), _means['test_acc'], yerr=_stds['test_acc'])
 
-    plt.xticks(x_pos, _means['model_params'], rotation=30)
 
-    plt.show()
+plt.tight_layout()
+plt.savefig('mean_test_acc.pdf')
+plt.show()
