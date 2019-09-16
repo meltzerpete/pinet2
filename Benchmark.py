71fc4bc19dfbc10493a5830d69c29a7c8c6cf756
pete meltzer <meltzerpete@gmail.com>
2019-09-16 13:06:45 +0100

    args + stdout stuff

--- Benchmark.py
+++ Benchmark.py
@@ -1,13 +1,16 @@
 import csv
 import sys
 import time
-
+import argparse
+import builtins
 import numpy as np
+# noinspection PyPackageRequirements
 import torch
+# noinspection PyPackageRequirements
+from torch.nn import CrossEntropyLoss
 from sklearn import metrics
 from sklearn.metrics import accuracy_score
 from sklearn.model_selection import StratifiedKFold
-from torch.nn import CrossEntropyLoss
 from torch_geometric.data import DataLoader
 from torch_geometric.datasets import TUDataset
 
@@ -15,7 +18,11 @@ from PiNet import PiNet
 
 
 def info(*msg):
-    print(msg, file=sys.stderr)
+    old_print(*msg, file=sys.stderr)
+
+
+old_print = builtins.print
+builtins.print = info
 
 
 def train():
@@ -59,11 +66,25 @@ def get_splits():
     return StratifiedKFold(n_splits=10).split(np.zeros([len(dataset), 1]), dataset.data.y)
 
 
+def get_args():
+    parser = argparse.ArgumentParser(description='10-fold Cross Validation.')
+    parser.add_argument('--dataset', dest='dataset_name', type=str,
+                        default='MUTAG', required=False,
+                        help='dataset name (default: MUTAG)')
+    parser.add_argument('--epochs', dest='epochs', type=int,
+                        default=500, required=False,
+                        help='sum the integers (default: 500)')
+
+    return parser.parse_args()
+
+
 if __name__ == '__main__':
+
+    args = get_args()
+    print(args.__dict__)
 
     writer = csv.writer(sys.stdout)
 
-    datasets = ['MUTAG', 'PTC_MM', 'PTC_MR', 'PTC_FM', 'PTC_FR', 'NCI1', 'NCI109', 'PROTEINS']
     models = [
         # {
         #     'class': PiNet,
@@ -135,7 +156,7 @@ if __name__ == '__main__':
     ]
 
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-    info(f'running on {device}')
+    print(f'running on {device}')
     writer.writerow(['dataset_name',
                      'model',
                      'model_params',
@@ -146,48 +167,47 @@ if __name__ == '__main__':
                      'train_acc', 'test_acc',
                      'train_conf', 'test_conf'])
 
-    for dataset_name in datasets:
-        for model_dict in models:
+    for model_dict in models:
 
-            dataset = TUDataset(root=f'data/{dataset_name}', name=dataset_name).shuffle()
+        dataset = TUDataset(root=f'data/{args.dataset_name}', name=args.dataset_name).shuffle()
 
-            for split, (all_train_idx, test_idx) in enumerate(get_splits()):
-                info(dataset_name, model_dict['class'].__name__, split)
+        for split, (all_train_idx, test_idx) in enumerate(get_splits()):
+            print(args.dataset_name, model_dict['class'].__name__, split)
 
-                model = model_dict['class'](num_feats=dataset.num_features, num_classes=dataset.num_classes,
-                                            **model_dict['params']).to(
-                    device)
-                optimizer = torch.optim.Adam(model.parameters(), lr=0.001, weight_decay=0.001)
-                crit = CrossEntropyLoss()
+            model = model_dict['class'](num_feats=dataset.num_features, num_classes=dataset.num_classes,
+                                        **model_dict['params']).to(
+                device)
+            optimizer = torch.optim.Adam(model.parameters(), lr=0.001, weight_decay=0.001)
+            crit = CrossEntropyLoss()
 
-                # convert idx to torch tensors
-                train_idx = torch.tensor(all_train_idx, dtype=torch.long)
-                test_idx = torch.tensor(test_idx, dtype=torch.long)
+            # convert idx to torch tensors
+            train_idx = torch.tensor(all_train_idx, dtype=torch.long)
+            test_idx = torch.tensor(test_idx, dtype=torch.long)
 
-                # train_idx, val_idx = next(StratifiedShuffleSplit(n_splits=1, test_size=0.1)
-                #                           .split(np.zeros([len(all_train_idx), 1]), dataset.data.y[all_train_idx]))
+            # train_idx, val_idx = next(StratifiedShuffleSplit(n_splits=1, test_size=0.1)
+            #                           .split(np.zeros([len(all_train_idx), 1]), dataset.data.y[all_train_idx]))
 
-                # train_idx = torch.tensor(train_idx, dtype=torch.long)
-                # val_idx = torch.tensor(val_idx, dtype=torch.long)
+            # train_idx = torch.tensor(train_idx, dtype=torch.long)
+            # val_idx = torch.tensor(val_idx, dtype=torch.long)
 
-                train_loader = DataLoader(dataset[train_idx], batch_size=len(train_idx))
-                # val_loader = DataLoader(dataset[val_idx], batch_size=len(val_idx))
-                test_loader = DataLoader(dataset[test_idx], batch_size=len(test_idx))
+            train_loader = DataLoader(dataset[train_idx], batch_size=len(train_idx))
+            # val_loader = DataLoader(dataset[val_idx], batch_size=len(val_idx))
+            test_loader = DataLoader(dataset[test_idx], batch_size=len(test_idx))
 
-                for epoch in range(500):
-                    start = time.time()
-                    train_loss = train()
-                    time_for_epoch = (time.time() - start) * 1e3
-                    train_acc, train_conf = evaluate(train_loader)
-                    # val_acc, val_conf = evaluate(val_loader)
-                    test_acc, test_conf = evaluate(test_loader)
+            for epoch in range(args.epochs):
+                start = time.time()
+                train_loss = train()
+                time_for_epoch = (time.time() - start) * 1e3
+                train_acc, train_conf = evaluate(train_loader)
+                # val_acc, val_conf = evaluate(val_loader)
+                test_acc, test_conf = evaluate(test_loader)
 
-                    writer.writerow([dataset_name,
-                                     model_dict["class"].__name__,
-                                     model_dict['params'],
-                                     split,
-                                     epoch,
-                                     time_for_epoch,
-                                     train_loss,
-                                     train_acc, test_acc,
-                                     train_conf, test_conf])
+                writer.writerow([args.dataset_name,
+                                 model_dict["class"].__name__,
+                                 model_dict['params'],
+                                 split,
+                                 epoch,
+                                 time_for_epoch,
+                                 train_loss,
+                                 train_acc, test_acc,
+                                 train_conf, test_conf])
